ec25ad76570056fbf160bae02bc33b2f5893db83
Dimela0 <dimelashukuru@gmail.com>
2025-01-21 18:05:22 -0500

    I'm submmitting my HTML and CSS code

--- css_1/Web  dev/script.js
+++
@@ -1,9 +0,0 @@
-function scrollLeft() {
-    const container = document.querySelector('.categories-wrapper');
-    container.scrollBy({ left: -300, behavior: 'smooth' });
-}
-
-function scrollRight() {
-    const container = document.querySelector('.categories-wrapper');
-    container.scrollBy({ left: 300, behavior: 'smooth' });
-}
